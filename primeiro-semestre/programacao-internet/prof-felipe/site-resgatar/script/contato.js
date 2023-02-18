8163cd828e3ff2185f88b8f39800ada03d5efb5b
m-matheus <matheus1999moraes@gmail.com>
2023-02-18 18:46:50 -0300

    formulario js

--- primeiro-semestre/programacao-internet/prof-felipe/site-resgatar/script/contato.js
+++ primeiro-semestre/programacao-internet/prof-felipe/site-resgatar/script/contato.js
@@ -5,18 +5,29 @@ var mensagemInput = document.getElementById("mensagem");
 var enviarSubmit = document.getElementById("enviar");
 
 nomeInput.addEventListener("focus", function() {
-  nomeInput.style.border = "3px solid #FF7A7D";
+  nomeInput.style.border = "3px solid #27221f";
 });
 
+nomeInput.addEventListener("click", function() {
+    nomeInput.style.backgroundColor = "#ffc7c8";
+  });
+
 emailInput.addEventListener("focus", function() {
-  emailInput.style.border = "3px solid #FF7A7D";
+  emailInput.style.border = "3px solid #27221f";
 });
+
+emailInput.addEventListener("click", function() {
+    emailInput.style.backgroundColor = "#ffc7c8";
+  });
 
 
 mensagemInput.addEventListener("focus", function() {
-  mensagemInput.style.border = "3px solid #FF7A7D";
+  mensagemInput.style.border = "3px solid #27221f";
 });
 
+mensagemInput.addEventListener("click", function() {
+    mensagemInput.style.backgroundColor = "#ffc7c8";
+  });
 
 enviarSubmit.addEventListener("submit", function(event) {
   event.preventDefault();
