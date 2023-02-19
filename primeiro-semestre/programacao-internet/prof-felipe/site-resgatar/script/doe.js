3cb2cbc7a3cf628af14428c6d7dd7b6abb5d7a6b
m-matheus <matheus1999moraes@gmail.com>
2023-02-18 21:24:39 -0300

    contribuicao com cards pronto

--- primeiro-semestre/programacao-internet/prof-felipe/site-resgatar/script/doe.js
+++ primeiro-semestre/programacao-internet/prof-felipe/site-resgatar/script/doe.js
@@ -1,8 +1,60 @@
-const doacaoFinanceira = document.getElementById("doacao-financeira");
-const card = document.getElementsByClassName("card");
+const doacaoFinanceira = document.querySelector("#financeira");
+const cardFinanceira = document.querySelector("#card-financeira");
+const hidenFinanceira = document.querySelector("#hiden-financeira");
 
 
 doacaoFinanceira.addEventListener("click", function(event) {
     event.preventDefault();
-    card.style.display = 'block';
+    cardFinanceira.style.display = 'block';
   });
+
+hidenFinanceira.addEventListener("click", function(event) {
+    event.preventDefault();
+    cardFinanceira.style.display = 'none';
+})
+
+
+const doacaoProdutos = document.querySelector("#produtos");
+const cardProdutos = document.querySelector("#card-produtos");
+const hidenProdutos = document.querySelector("#hiden-produtos");
+
+
+doacaoProdutos.addEventListener("click", function(event) {
+    event.preventDefault();
+    cardProdutos.style.display = 'block';
+  });
+
+hidenProdutos.addEventListener("click", function(event) {
+    event.preventDefault();
+    cardProdutos.style.display = 'none';
+})
+
+const doacaoTempo = document.querySelector("#tempo");
+const cardTempo = document.querySelector("#card-tempo");
+const hidenTempo = document.querySelector("#hiden-tempo");
+
+
+doacaoTempo.addEventListener("click", function(event) {
+    event.preventDefault();
+    cardTempo.style.display = 'block';
+  });
+
+hidenTempo.addEventListener("click", function(event) {
+    event.preventDefault();
+    cardTempo.style.display = 'none';
+})
+
+const doacaoAdocao = document.querySelector("#adocao");
+const cardAdocao = document.querySelector("#card-adocao");
+const hidenAdocao = document.querySelector("#hiden-adocao");
+
+
+doacaoAdocao.addEventListener("click", function(event) {
+    event.preventDefault();
+    cardAdocao.style.display = 'block';
+  });
+
+hidenAdocao.addEventListener("click", function(event) {
+    event.preventDefault();
+    cardAdocao.style.display = 'none';
+})
