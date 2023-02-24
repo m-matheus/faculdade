a268fdbad78a2e0c257d5c1af3e0f73dfc8222d5
m-matheus <matheus1999moraes@gmail.com>
2023-02-23 22:48:21 -0300

    responsividade

---
+++ primeiro-semestre/programacao-internet/prof-felipe/site-resgatar/script/home.js
@@ -0,0 +1,7 @@
+const gatoLaranja = document.querySelector("#gatinho-ruivo");
+
+gatoLaranja.addEventListener("mouseenter", function(event) {
+    event.preventDefault();
+    gatoLaranja.style.sha = "400px";
+  });
+
